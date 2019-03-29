a16007529ded710b24cc7f5c1ae1890c1804ac4a
Norman Basham <nbasham@yahoo.com>
2019-03-29 12:45:11 -0700

    Removed unused line of code.

--- Navigator/Source/AppDelegate.swift
+++ Navigator/Source/AppDelegate.swift
@@ -7,7 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window?.rootViewController?.navigationController
         if isNotUnitTesting() {
             appCoordinator = AppCoordinator(appNavigator: Navigator(), dataService: DataServiceImpl())
             appCoordinator.start()
